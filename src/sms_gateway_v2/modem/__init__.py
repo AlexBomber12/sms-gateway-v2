ab1728d39c6eecd9c27b828626084ca87bc6df67
AlexBomber12 <AlexBomber12@users.noreply.github.com>
2026-04-26 02:37:07 +0200

    feat: add modem exceptions

---
+++ src/sms_gateway_v2/modem/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from sms_gateway_v2.modem.exceptions import (
+    MessageDeleteFailed,
+    ModemBusy,
+    ModemError,
+    ModemManagerUnavailable,
+    ModemNotFound,
+)
+
+__all__ = [
+    "MessageDeleteFailed",
+    "ModemBusy",
+    "ModemError",
+    "ModemManagerUnavailable",
+    "ModemNotFound",
+]
